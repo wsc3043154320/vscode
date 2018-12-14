261383e4e9a57ea9fb56469639c5ae43134b60b0
wsc3043154320 <3l>
2018-12-14 12:39:21 +0800

    d:\show\test.sql

--- test.sql
+++ test.sql
@@ -1,4 +1,2 @@
 select deal_check.data,deal_check.金额，username from deal_check，deal_order
-where username  in (select username from user);funck
-
-的撒 的威风啊发我范围阿风啊额伟大飞啊飞v费瓦二点v改变
+where username  in (select username from user);ffa da fas fdsaf as fdsa fas 
